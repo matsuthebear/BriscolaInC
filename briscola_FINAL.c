c63da439582f8ff7efe0a764ae2012c950391595
Matteo Marzio <matteomarzio@gmail.com>
2018-02-08 10:05:18 +0100

    Final Version
    Versione finale del programma. Corretti tutti i bug.

--- briscola_FINAL.c
+++ briscola_FINAL.c
@@ -1,10 +1,19 @@
 /*
   Author : Matteo Marzio
   Start Date : 07-02-2018
-  Finish Date : xx-xx-2018
-  Current Version : 1.0
+  Finish Date : 08-02-2018
+  Current Version : 1.2
 
   ChangeLog:
+    Version 1.2
+    ---- Final Build
+    ---- Ultime correzioni
+    ---- Parti debug commentate e nascoste all'utente finale
+    ---- Deployment
+      ----Correzione Bug
+    Version 1.1
+    ---- Miglioramento Stampa valori
+    ---- Ancora presente il bug dell' 11esimo valore
     Version 1.0
     ----Initial Build
 
@@ -69,7 +78,9 @@
 
 //COSTANTI
 #define CARTE 40
-
+// VARIABILI GLOBALI
+// Tiene conto delle carte ancora in gioco
+int carte_rimanenti = 0;
 //FUNZIONI
 // Funzione che setta di default ad ogni partita un nuovo mazzo di carte.
 void default_carte(int carte[4][10]){
@@ -146,9 +157,9 @@ void stampa_mano(int mano[2][3]){
 //Funzione che gestisce le carte tra il mazzo e le due mani
 void random_pick(int ultima_posizione, int mano[2][3], int carte[4][10]){
   int controllo = 1;
-  while(controllo){
+  while(controllo && carte_rimanenti < 34){
     int seme = rand() % 4; //considero solamente il seme della lista
-    int valore = rand() % 11;
+    int valore = rand() % 10;
     if (carte[seme][valore] != 0){
       mano[0][ultima_posizione] = seme + 1;
       mano[1][ultima_posizione] = valore + 1;
@@ -156,37 +167,60 @@ void random_pick(int ultima_posizione, int mano[2][3], int carte[4][10]){
       controllo = 0;
     }
   }
+  if (carte_rimanenti >= 34){
+    mano[0][ultima_posizione] = 0;
+    mano[1][ultima_posizione] = 0;
+  }
 }
 
-int convertitore_intero_punto(int carta ){
+
+int convertitore_intero_punto(int carta, int gestione ){
   int punto;
-  switch (carta){
-    //Asso
-    case 1:
-      punto = 11;
-      break;
-    //Tre
-    case 3:
-      punto = 10;
-      break;
-    //fante
-    case 8:
-      punto = 2;
-      break;
-    //Cavallo
-    case 9:
-      punto = 3;
-      break;
-    //re
-    case 10:
-      punto = 4;
-      break;
-    //le altre carte non hanno punti
-    default:
-      punto = 0;
-  }
+  // se e' uno devo calcolare il punteggio, se 0 devo semplificare la comparazione
+  if (gestione == 1){
+      switch (carta){
+        //Asso
+        case 1:
+          punto = 11;
+          break;
+        //Tre
+        case 3:
+          punto = 10;
+          break;
+        //fante
+        case 8:
+          punto = 2;
+          break;
+        //Cavallo
+        case 9:
+          punto = 3;
+          break;
+        //re
+        case 10:
+          punto = 4;
+          break;
+        //le altre carte non hanno punti
+        default:
+          punto = 0;
+      }
+    }
+  if (gestione == 0 ){
+    switch (carta){
+      //Asso
+      case 1:
+        punto = 12;
+        break;
+      //Tre
+      case 3:
+        punto = 11;
+        break;
+      default:
+        punto = carta;
+    }
+ }
   return punto;
 }
+
 //MAIN
 int main(){
   srand(time(NULL));
@@ -208,8 +242,6 @@ int main(){
   int seme_partita = rand() % 4 + 1;
   int seme_partita_temp;
   //Permette di indicare le carti rimanenti. Inizia da 6 perche'
-  //si tolgono via 6 carte per il turno iniziale
-  int carte_rimanenti = 6;
   //Punto del turno
   int punto = 0;
   //vittoria del turno precedente
@@ -229,7 +261,7 @@ int main(){
   printf("La briscola di questa partita e' : ");
   print_seme(seme_partita);
   printf("\n");
-  debug_carte(carte);
+  //debug_carte(carte);
   printf("\n");
   //Si danno le carte
   for (i = 0; i < 3; i++)
@@ -238,23 +270,30 @@ int main(){
     random_pick(i,mano_cpu,carte);
 
 
-  while((CARTE - carte_rimanenti) != 0){
-      printf("\n");
-      debug_carte(carte);
-      printf("\n");
+  while((CARTE - carte_rimanenti) > 0){
+      //printf("\n");
+      //debug_carte(carte);
+      //printf("\n");
       //La CPU butta la sua carta
-      i = rand() % 3;
-      seme_cpu = mano_cpu[0][i];
-      valore_cpu = mano_cpu[1][i];
+      do{
+        i = rand() % 3;
+        seme_cpu = mano_cpu[0][i];
+        valore_cpu = mano_cpu[1][i];
+      }while(seme_cpu == 0);
+
       //Selezione della Carta
       stampa_mano(mano_giocatore);
-      stampa_mano(mano_cpu);
+      //stampa_mano(mano_cpu);
 
-      printf("Che carta scegli? 1, 2 o 3? \n");
-      scanf("%i", &carta_giocatore);
-      carta_giocatore--;
-      seme_giocatore = mano_giocatore[0][carta_giocatore];
-      valore_giocatore = mano_giocatore[1][carta_giocatore];
+      do{
+        printf("Che carta scegli? 1, 2 o 3? \n");
+        scanf("%i", &carta_giocatore);
+        carta_giocatore--;
+        seme_giocatore = mano_giocatore[0][carta_giocatore];
+        valore_giocatore = mano_giocatore[1][carta_giocatore];
+      }while(carta_giocatore < 0 || carta_giocatore > 3 || seme_giocatore == 0);
+      //La posizione ovviamente deve corrispondere alla posizione nella lista
+
       /*
         Dobbiamo considerare diversi casi:
         -- I due giocatori hanno lo stesso seme, e quindi non mi interessa della
@@ -265,6 +304,13 @@ int main(){
               precedente ha la precedenza. Consideriamo come vincitore all'inizio
               sempre il giocatore
       */
+
+      punto = convertitore_intero_punto(valore_giocatore, 1) + convertitore_intero_punto(valore_cpu, 1);
+
+      valore_giocatore = convertitore_intero_punto(valore_giocatore, 0);
+      valore_cpu = convertitore_intero_punto(valore_cpu, 0);
+
+
       //Primo caso : entrambi hanno lo stesso seme
       if (seme_giocatore == seme_cpu){
         if(valore_giocatore < valore_cpu){
@@ -276,18 +322,18 @@ int main(){
         }
       }
       //Entrambi hanno carte diverse ma uno ha o la briscola o la precedenza
-      else if (seme_giocatore != seme_cpu){
+      else{
         //Secondo Caso : Il primo giocatore non ha la briscola e nemmeno la cpu
         if (seme_giocatore != seme_partita && seme_cpu != seme_partita){
           //Il giocatore ha la precedenza, la cpu non ha ne' la briscola ne' la precedenza
-          if (seme_giocatore == seme_partita_temp && seme_cpu != seme_partita_temp && seme_cpu != seme_partita){
+          if (vittoria == 1){
             punteggio_giocatore += punto;
             vittoria = 1;
-          //La cpu ha la briscola, mentre il giocatore non ha la precedenza
-          }else if (seme_cpu == seme_partita_temp && seme_giocatore != seme_partita_temp){
+            }else{
               punteggio_CPU += punto;
               vittoria = 0;
-            }
+          }
+
         //sapendo che i due semi non possono essere uguali QUA, so gia' che ha vinto
         // il giocatore / cpu
         }else if (seme_giocatore == seme_partita){
@@ -298,6 +344,8 @@ int main(){
           vittoria = 0;
         }
       }
+      valore_giocatore = mano_giocatore[1][carta_giocatore];
+      valore_cpu = mano_cpu[1][i];
       printf("\nIl computer mette : ");
       print_seme(seme_cpu);
       print_valore(valore_cpu);
@@ -305,26 +353,38 @@ int main(){
       print_seme(seme_giocatore);
       print_valore(valore_giocatore);
 
-      printf("\nVittoria : %i\n\n", vittoria);
+      //printf("\nVittoria : %i\n\n", vittoria);
 
       /*
         Nel caso di vittoria, il giocatore pesca prima la carta. In caso di sconfitta,
         il contrario
       */
       if (vittoria == 1){
+          printf("\nIl giocatore vince il turno!\n\n");
           random_pick(carta_giocatore,mano_giocatore,carte);
           random_pick(i,mano_cpu,carte);
       }
       if (vittoria == 0){
+          printf("\nIl computer vince il turno!\n\n");
           random_pick(i,mano_cpu,carte);
           random_pick(carta_giocatore,mano_giocatore,carte);
       }
       //Informa al ciclo che mancano due carte
       carte_rimanenti += 2;
+      //printf("Punto : %i  Giocatore : %i  CPU : %i\n" , punto, punteggio_giocatore, punteggio_CPU);
   }
 
-  printf("PUNTEGGIO \n");
-  printf("Giocatore : %i  CPU : %i" , punteggio_giocatore, punteggio_CPU);
+  printf("PUNTEGGIO FINALE\n");
+  printf("Giocatore : %i  CPU : %i\n" , punteggio_giocatore, punteggio_CPU);
+  if (punteggio_giocatore != punteggio_CPU){
+    if (vittoria == 1)
+        printf("\nIl giocatore ha vinto la partita!\n\n");
+    else
+        printf("\nIl computer ha vinto la partita!\n\n");
+  }else{
+    printf("C'e' stato un pareggio!\n\n");
+  }
+
 
 
 
